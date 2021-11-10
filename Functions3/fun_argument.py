e2bc5e98414229235efe21ae78a0c5bacd452795
Mastermindx33 <mastermindx33@gmail.com>
2021-11-10 23:44:57 +0530

    deploy @fn3

---
+++ Functions3/fun_argument.py
@@ -0,0 +1,5 @@
+def hello(name):
+    print(f"Hello {name}")
+
+hello("Blesslin")
+hello("Jerish")
---
+++ Functions3/fun_hello.py
@@ -0,0 +1,9 @@
+def hello():
+    print("Hola")
+    print("Bonjour")
+    print("Vanakam Maplae Kakooslandu")
+
+
+hello()
+hello()
+hello()

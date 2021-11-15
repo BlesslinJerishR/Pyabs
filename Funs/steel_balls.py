75d39537402f27a20a8c4489737b67a247e4cc99
Mastermindx33 <mastermindx33@gmail.com>
2021-11-15 16:03:35 +0530

    Update steel_balls.py

--- Funs/steel_balls.py
+++ Funs/steel_balls.py
@@ -1,5 +1,22 @@
 import random
 
+
 def balls(ball):
     if ball == 1:
         return "It is certain"
+    elif ball == 2:
+        return "It is lighta"
+    elif ball == 3:
+        return "It is vaipu iruku"
+    elif ball == 4:
+        return "It is pakalam"
+    elif ball == 5:
+        return "It is solren"
+    elif ball == 6:
+        return "It is apdiaa"
+    elif ball == 7:
+        return "It is seri"
+    elif ball == 8:
+        return "It is kekren"
+    elif ball == 9:
+        return "It is vaipilla raja"
